3a906d2363d2b639bb8d57827a23aa2a86996461
SeppoT <seppo.torniainen@happywise.fi>
2019-09-19 21:23:04 +0300

    database tests updated

--- test/test_app.py
+++ test/test_app.py
@@ -70,6 +70,7 @@ def test_create_instances(db_handle):
 
     db_handle.session.commit();
 
+    #check that tables exist
     assert User.query.count() == 1
     assert CourseMedia.query.count() == 1
     assert TrainingCourse.query.count() == 1
@@ -84,6 +85,16 @@ def test_create_instances(db_handle):
 
 def test_user_columns(db_handle):
     
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(30))
+    lastname = db.Column(db.String(30))
+    email = db.Column(db.String(100))
+    isAdmin = db.Column(db.Boolean, nullable=False)
+    creationdate = db.Column(db.DateTime)
+    courses = db.relationship("TrainingCourse",secondary=courseuserrelation,back_populates="users")
+    '''
+
     print("App+Db test, test user columns")
     #test user has isAdmin flag set
     user = User()
@@ -93,7 +104,52 @@ def test_user_columns(db_handle):
 
     db_handle.session.rollback()
 
+    user = User()
+    user.isAdmin=True;
+    db_handle.session.add(user)
+    db_handle.session.commit()
+
+    db_handle.session.rollback()
+
+    user = User()
+    user.isAdmin=True;
+    user.firstname='testi1'
+    user.lastname='testi2'
+    user.email='testemail@com'    
+    db_handle.session.add(user)
+    db_handle.session.commit()
+
+    print("test user update and delete")
+    print(user.id)
+    getuser = User.query.filter_by(id=user.id).first()
+    assert getuser.firstname == 'testi1'
+
+    getuser.firstname = 'nameedited'
+    db_handle.session.add(getuser)
+    db_handle.session.commit()
+    getuser2 = User.query.filter_by(id=getuser.id).first()
+    with pytest.raises(AssertionError):
+        assert getuser2.firstname == 'nameeditedwrong'
+
+    db_handle.session.delete(getuser2)
+    db_handle.session.commit()
+    getuser3 = User.query.filter_by(id=getuser2.id).first()
+    #check if deleted
+    with pytest.raises(AttributeError):
+        assert getuser3.firstname == 'nameedited'
+
+
 def test_course_columns(db_handle):
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    creationdate = db.Column(db.DateTime)
+    startdate = db.Column(db.DateTime)
+    enddate = db.Column(db.DateTime)
+    coursedatajson = db.Column(db.String)
+    medialist = db.relationship("CourseMedia",backref="trainingcourse", lazy=True)
+    users = db.relationship("User",secondary=courseuserrelation,back_populates="courses")
+    '''
 
     print("App+Db test, test course columns")
     #test that course name is unique
@@ -105,4 +161,67 @@ def test_course_columns(db_handle):
         db_handle.session.commit()
 
     db_handle.session.rollback()
+
+    course = TrainingCourse()
+    course.isAdmin=True;
+    db_handle.session.add(course)
+    db_handle.session.commit()
+
+    db_handle.session.rollback()
+
+    course = TrainingCourse()
+    course.name='testi1'
+    db_handle.session.add(course)
+    db_handle.session.commit()
+
+    print("test course update and delete")
+    print(course.id)
+    getcourse = TrainingCourse.query.filter_by(id=course.id).first()
+    assert getcourse.name == 'testi1'
+
+    getcourse.name = 'nameedited'
+    db_handle.session.add(getcourse)
+    db_handle.session.commit()
+    getcourse2 = TrainingCourse.query.filter_by(id=getcourse.id).first()
+    with pytest.raises(AssertionError):
+        assert getcourse2.name == 'nameeditedwrong'
+
+    db_handle.session.delete(getcourse2)
+    db_handle.session.commit()
+    getcourse3 = TrainingCourse.query.filter_by(id=getcourse2.id).first()
+    #check if deleted
+    with pytest.raises(AttributeError):
+        assert getcourse3.name == 'nameedited'
+
+def test_coursemedia_columns(db_handle):
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255))
+    type = db.Column(db.String(20))
+    course_id = db.Column(db.Integer, db.ForeignKey('TrainingCourse.id', ondelete="SET NULL"))
+    '''
+    print("App+Db test, test coursemedia columns")
+    media = CourseMedia()
+    media.url='testi1'
+    db_handle.session.add(media)
+    db_handle.session.commit()
+
+    print("test media update and delete")
+    print(media.id)
+    getmedia = CourseMedia.query.filter_by(id=media.id).first()
+    assert getmedia.url == 'testi1'
+
+    getmedia.url = 'nameedited'
+    db_handle.session.add(getmedia)
+    db_handle.session.commit()
+    getmedia2 = CourseMedia.query.filter_by(id=getmedia.id).first()
+    with pytest.raises(AssertionError):
+        assert getmedia2.url == 'nameeditedwrong'
+
+    db_handle.session.delete(getmedia2)
+    db_handle.session.commit()
+    getmedia3 = CourseMedia.query.filter_by(id=getmedia2.id).first()
+    #check if deleted
+    with pytest.raises(AttributeError):
+        assert getmedia3.url == 'nameedited'
 
